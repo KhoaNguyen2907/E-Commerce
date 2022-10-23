fd5b47fffa6685c0dab8925dc33f48e7a1287a4e
KhoaNguyen2907 <99467514+KhoaNguyen2907@users.noreply.github.com>
2022-10-23 15:52:15 +0700

    Permit add user without login

--- src/main/java/com/ckt/ecommercecybersoft/security/config/WebSecurityConfig.java
+++ src/main/java/com/ckt/ecommercecybersoft/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.ckt.ecommercecybersoft.security.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -45,6 +46,7 @@ public class WebSecurityConfig {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/v1/users").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/api/v1/**").authenticated()
                 .anyRequest().permitAll()
