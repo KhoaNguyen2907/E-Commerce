946d8c1c92595cd77b7691c554c3151d57cad9e9
cuongpm58 <cuongpm5295@gmail.com>
2022-10-30 22:20:19 +0700

    create table for order, order-item, address

---
+++ src/main/java/com/ckt/ecommercecybersoft/order/constant/OrderConstant.java
@@ -0,0 +1,23 @@
+package com.ckt.ecommercecybersoft.order.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class OrderConstant {
+    @UtilityClass
+    public class Order {
+        public static final String TABLE_NAME = "order_table";
+        public static final String COMMENT = "comment";
+        public static final String NAME = "name";
+        public static final String STATUS = "status";
+        public static final String TOTAL_COST = "total_cost";
+        public static final String TYPE = "type";
+    }
+
+    @UtilityClass
+    public class OrderItem {
+        public static final String TABLE_NAME = "order_item";
+        public static final String QUANTITY = "quantity";
+        public static final String COST = "cost";
+    }
+}
---
+++ src/main/java/com/ckt/ecommercecybersoft/order/constant/OrderUrl.java
@@ -0,0 +1,5 @@
+package com.ckt.ecommercecybersoft.order.constant;
+
+public class OrderUrl {
+    public static final String URL_ORDER = "/api/v1/order";
+}
