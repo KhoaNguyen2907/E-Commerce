9a059fa1f6f765c16adebce92d8d8579347ba33a
cuongpm58 <cuongpm5295@gmail.com>
2022-10-30 22:51:43 +0700

    update ResponseDTO format

--- src/main/java/com/ckt/ecommercecybersoft/common/utils/ResponseUtils.java
+++ src/main/java/com/ckt/ecommercecybersoft/common/utils/ResponseUtils.java
@@ -2,9 +2,11 @@ package com.ckt.ecommercecybersoft.common.utils;
 
 import com.ckt.ecommercecybersoft.common.exception.NotFoundException;
 import com.ckt.ecommercecybersoft.common.model.ResponseDTO;
+import com.ckt.ecommercecybersoft.common.model.Error;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -24,56 +26,74 @@ import java.util.stream.Collectors;
  * @author KhoaNguyen
  * @version 1.0
  * @since 2022-10-27
- *
  */
 
 public class ResponseUtils {
 
-    public static ResponseEntity<ResponseDTO> get (Object data, HttpStatus status) {
+    public static ResponseEntity<ResponseDTO> get(Object data, HttpStatus status) {
         ResponseDTO response = ResponseDTO.builder()
                 .content(data)
-                .hasErrors(false)
                 .errors(null)
                 .timestamp(DateTimeUtils.now())
                 .status(status.value())
+                .isContentArray(false)
+                .build();
+        return new ResponseEntity<>(response, status);
+    }
+
+    public static ResponseEntity<ResponseDTO> get(List<Object> data, HttpStatus status) {
+        ResponseDTO response = ResponseDTO.builder()
+                .content(data)
+                .errors(null)
+                .timestamp(DateTimeUtils.now())
+                .status(status.value())
+                .isContentArray(true)
                 .build();
         return new ResponseEntity<>(response, status);
     }
 
     public static ResponseEntity<ResponseDTO> error(ConstraintViolationException exception, HttpStatus status) {
 
-        List<String> errors = exception.getConstraintViolations().stream()
-                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
-        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true).errors(errors)
+        List<Error> errors = exception.getConstraintViolations().stream()
+                .map(constraintViolation ->
+                        new Error(0, constraintViolation.getMessage()))
+                .collect(Collectors.toList());
+        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true)
+                .errors(List.of(new Error(0, exception.getMessage())))
                 .timestamp(DateTimeUtils.now()).status(status.value()).build();
         return new ResponseEntity<>(response, status);
     }
 
     public static ResponseEntity<ResponseDTO> error(MethodArgumentNotValidException exception, HttpStatus status) {
-        List<String> errors = exception.getAllErrors().stream()
-                .map(e -> e.getDefaultMessage()).collect(Collectors.toList());
-        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true).errors(errors)
+        List<Error> errors = exception.getAllErrors().stream()
+                .map(e -> new Error(0, e.getDefaultMessage()))
+                .collect(Collectors.toList());
+        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true)
+                .errors(List.of(new Error(0, exception.getMessage())))
                 .timestamp(DateTimeUtils.now()).status(status.value()).build();
         return new ResponseEntity<>(response, status);
     }
 
     public static ResponseEntity<ResponseDTO> error(NotFoundException exception, HttpStatus status) {
         String error = exception.getMessage();
-        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true).errors(List.of(error))
+        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true)
+                .errors(List.of(new Error(0, error)))
                 .timestamp(DateTimeUtils.now()).status(status.value()).build();
         return new ResponseEntity<>(response, status);
     }
 
     public static ResponseEntity<ResponseDTO> error(DisabledException exception, HttpStatus status) {
         String error = exception.getMessage();
-        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true).errors(List.of(error))
+        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true)
+                .errors(List.of(new Error(0, error)))
                 .timestamp(DateTimeUtils.now()).status(status.value()).build();
         return new ResponseEntity<>(response, status);
     }
 
     public static ResponseEntity<ResponseDTO> error(BadCredentialsException exception, HttpStatus status) {
         String error = "Wrong username or password";
-        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true).errors(List.of(error))
+        ResponseDTO response = ResponseDTO.builder().content(null).hasErrors(true)
+                .errors(List.of(new Error(0, error)))
                 .timestamp(DateTimeUtils.now()).status(status.value()).build();
         return new ResponseEntity<>(response, status);
     }
